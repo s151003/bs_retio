93b0ab9260448a1406503889053a8ce6240e332b
s151003 <s151003@kashiwanoha.ed.jp>
2017-12-07 14:42:32 +0000

    [Update] 関数にした
    こちらでは #2 が起きなかった
    スクレイピングの仕方がいけないのかな

--- xm.py
+++ xm.py
@@ -1,32 +1,43 @@
-import urllib2
-from bs4 import BeautifulSoup
+def xm():
+	import urllib2
+	from bs4 import BeautifulSoup
 
-url = "http://www.xmtrading.com/jp/"
-headers = 'Mozilla/5.0'
+	url = "http://www.xmtrading.com/jp/"
+	headers = 'Mozilla/5.0'
 
-req = urllib2.Request(url)
-req.add_header("User-agent", headers)
+	req = urllib2.Request(url)
+	req.add_header("User-agent", headers)
 
-soup = BeautifulSoup(urllib2.urlopen(req))
+	soup = BeautifulSoup(urllib2.urlopen(req))
 
-# symbol
-symbol = []
-divs = soup.find("div", id="dashboard-wrap").find_all("b")
-for div in divs:
-	symbol = symbol + [div.string]
+	# symbol
+	symbol = []
+	divs = soup.find("div", id="dashboard-wrap").find_all("b")
+	for div in divs:
+		symbol = symbol + [div.string]
+		
+	# buy
+	b_retio = []
+	links = soup.find_all("i", class_="green-bar-nbr")
+	for i in links:
+		b_retio = b_retio + [i.string]
+
+	# sell	
+	s_retio = []
+	links = soup.find_all("i", class_="red-bar-nbr")
+	for i in links:
+		s_retio = s_retio + [i.string]
 	
-# buy
-b_retio = []
-links = soup.find_all("i", class_="green-bar-nbr")
-for i in links:
-	b_retio = b_retio + [i.string]
+	return symbol, b_retio, s_retio
 
-# sell	
-s_retio = []
-links = soup.find_all("i", class_="red-bar-nbr")
-for i in links:
-	s_retio = s_retio + [i.string]
+xmSym, xmB, xmS = xm()
 
-print symbol
-print b_retio	
-print s_retio
+#oandaS.append("haneisaren")
+
+count = 0
+for o in xmSym:
+	print o
+	print "B:",xmB[count]
+	print "S:",xmS[count]
+	count = count + 1
+print 
