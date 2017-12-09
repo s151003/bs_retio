3fa04ba3b7b22a76f76ed0f3805a2f01978cae63
s151003 <s151003@kashiwanoha.ed.jp>
2017-12-09 16:10:52 +0900

    [Update] ig.py 外為どっとこむ関数

---
+++ ig.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+def gaitame():
+	from selenium import webdriver
+	from bs4 import BeautifulSoup
+	from time import sleep
+
+	driver = webdriver.PhantomJS()
+	driver.get('http://www.gaitame.com/market/')
+	driver.set_page_load_timeout(5)
+	driver.find_element_by_class_name('ga06-clone')
+	html = driver.page_source
+
+	soup = BeautifulSoup(html)
+	div = soup.find("div", id="ga06_1")
+
+	#symbol
+	symbol = []
+	spans = div.find_all('img')
+	for span in spans:
+		symbol.append(span['alt'])
+
+
+
+	#long
+	b_retio = []
+	spans = div.find_all("span", class_="buyP")
+	for span in spans:
+		b_retio.extend(span.stripped_strings)
+
+	# short
+	s_retio = []
+	spans = div.find_all("span", class_="sellP")
+	for span in spans:
+		s_retio.extend(span.stripped_strings)
+
+	driver.quit()
+	return symbol,b_retio,s_retio
